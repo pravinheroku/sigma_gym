745928f4bbe43e6a05bd21a2c32ff8a117f26b8c
stupravin@gmail.com <stupravin@gmail.com>
2024-03-19 11:04:09 +0530

    settings updated

--- pravingym/pravingym/settings.py
+++ pravingym/pravingym/settings.py
@@ -124,7 +124,7 @@ STATIC_URL = "/static/"
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-STATIC_ROOT = BASE_DIR / "assets"
+STATIC_ROOT = BASE_DIR / "static"
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedMainfestStaticFilesStorage"
 
