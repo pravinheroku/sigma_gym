cfee65935f2edba96b8c419cc385060b085385af
stupravin@gmail.com <stupravin@gmail.com>
2024-03-19 10:58:39 +0530

    manage.py setting updated

--- pravingym/pravingym/wsgi.py
+++ pravingym/pravingym/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pravingym.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pravingym.pravingym.settings')
 
 application = get_wsgi_application()
